1b138c0393d238f2499309304d88fc947bdb04cf
sulekhame80-tech <sulekhame80@gmail.com>
2026-04-09 21:42:54 +0530

    Refactor Content component to functional component

--- src/components/sections/dashboard/web-analytics/Content.js
+++ src/components/sections/dashboard/web-analytics/Content.js
@@ -1,60 +1,42 @@
-import React, { Component } from 'react';
-import Averagenycountry from '../../home/Averagenycountry';
+import React from 'react';
 import GoogleAnalytics from './Googleanalytics';
-import Topbar from './Topbar';
-import Topuser from './Topuser';
-import Websiteaudience from './Websiteaudience';
 import PaymentSummaryTable from './Paymentsummary';
 import LabRecordTable from './labrecord';
 import ReportSummary from './reporttable';
 import DashboardCalendar from './DashboardCalendar';
 
-class Content extends Component {
-  render() {
-    return (
-      <>
-        <div className="ms-content-wrapper">
-          {/* Top Metric Cards (Entire Screen Width) */}
-          <div className="row no-gutters">
-            <div className="col-md-12">
-              <PaymentSummaryTable />
-            </div>
-          </div>
-
-          <div className="row mt-3">
-            <div className="col-xl-7 col-md-12">
-              
-              <div className="mt-3">
-                <ReportSummary />
-              </div>
-            </div>
-
-            <div className="col-xl-5 col-md-12">
-              <DashboardCalendar />
-              <div className="mt-3">
-                <GoogleAnalytics />
-              </div>
-            </div>
-          </div>
-
-          {/* LabRecordTable and ReportSummary remain below */}
-          <div className="row">
-            <div className="col-12 mt-3">
-              <LabRecordTable />
-            </div>
-          </div>
-
-          {/* Existing second row unchanged 
-          <div className="row">
-            <Topuser />
-            <div className="col-xl-7 col-md-12">
-              <Averagenycountry />
-            </div>
-          </div>*/}
+const Content = () => {
+  return (
+    <div className="ms-content-wrapper">
+      
+      {/* Top Metric Cards */}
+      <div className="row g-0">
+        <div className="col-md-12">
+          <PaymentSummaryTable />
         </div>
-      </>
-    );
-  }
-}
+      </div>
+
+      <div className="row mt-3 g-3">
+        <div className="col-xl-7 col-md-12">
+          <ReportSummary />
+        </div>
+
+        <div className="col-xl-5 col-md-12">
+          <DashboardCalendar />
+          <div className="mt-3">
+            <GoogleAnalytics />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-12 mt-3">
+          <LabRecordTable />
+        </div>
+      </div>
+
+    </div>
+  );
+};
 
 export default Content;
