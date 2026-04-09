8ea25caf5979c5a0eb65ba82ac4b20d11e7dfaf6
sulekhame80-tech <sulekhame80@gmail.com>
2026-04-09 18:36:06 +0530

    Update Content.js

--- src/components/sections/dashboard/web-analytics/Content.js
+++ src/components/sections/dashboard/web-analytics/Content.js
@@ -12,45 +12,47 @@ import DashboardCalendar from './DashboardCalendar';
 class Content extends Component {
   render() {
     return (
-      <div className="ms-content-wrapper">
-        {/* Top Metric Cards (Entire Screen Width) */}
-        <div className="row no-gutters">
-          <div className="col-md-12">
-            <PaymentSummaryTable />
-          </div>
-        </div>
-
-        <div className="row mt-3">
-          <div className="col-xl-7 col-md-12">
-            <Websiteaudience />
-            <div className="mt-3">
-              <ReportSummary />
+      <>
+        <div className="ms-content-wrapper">
+          {/* Top Metric Cards (Entire Screen Width) */}
+          <div className="row no-gutters">
+            <div className="col-md-12">
+              <PaymentSummaryTable />
             </div>
           </div>
 
-          <div className="col-xl-5 col-md-12">
-            <DashboardCalendar />
-            <div className="mt-3">
-              <GoogleAnalytics />
+          <div className="row mt-3">
+            <div className="col-xl-7 col-md-12">
+              <Websiteaudience />
+              <div className="mt-3">
+                <ReportSummary />
+              </div>
+            </div>
+
+            <div className="col-xl-5 col-md-12">
+              <DashboardCalendar />
+              <div className="mt-3">
+                <GoogleAnalytics />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* LabRecordTable and ReportSummary remain below */}
-        <div className="row">
-          <div className="col-12 mt-3">
-            <LabRecordTable />
+          {/* LabRecordTable and ReportSummary remain below */}
+          <div className="row">
+            <div className="col-12 mt-3">
+              <LabRecordTable />
+            </div>
           </div>
-        </div>
 
-        {/* Existing second row unchanged 
-        <div className="row">
-          <Topuser />
-          <div className="col-xl-7 col-md-12">
-            <Averagenycountry />
-          </div>
-        </div>*/}
-      </div>
+          {/* Existing second row unchanged 
+          <div className="row">
+            <Topuser />
+            <div className="col-xl-7 col-md-12">
+              <Averagenycountry />
+            </div>
+          </div>*/}
+        </div>
+      </>
     );
   }
 }
