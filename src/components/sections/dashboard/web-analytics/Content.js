523d446267db7d4b12788d6403f22af6117d19c2
sulekhame80-tech <sulekhame80@gmail.com>
2026-04-09 21:44:40 +0530

    Refactor Content component to use class syntax

--- src/components/sections/dashboard/web-analytics/Content.js
+++ src/components/sections/dashboard/web-analytics/Content.js
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Averagenycountry from '../../home/Averagenycountry';
 import GoogleAnalytics from './Googleanalytics';
+import Topbar from './Topbar';
+import Topuser from './Topuser';
+import Websiteaudience from './Websiteaudience';
 import PaymentSummaryTable from './Paymentsummary';
 import LabRecordTable from './labrecord';
 import ReportSummary from './reporttable';
 import DashboardCalendar from './DashboardCalendar';
 
-const Content = () => {
-  return (
-    <div className="ms-content-wrapper">
-      
-      {/* Top Metric Cards */}
-      <div className="row g-0">
-        <div className="col-md-12">
-          <PaymentSummaryTable />
-        </div>
-      </div>
-
-      <div className="row mt-3 g-3">
-        <div className="col-xl-7 col-md-12">
-          <ReportSummary />
-        </div>
-
-        <div className="col-xl-5 col-md-12">
-          <DashboardCalendar />
-          <div className="mt-3">
-            <GoogleAnalytics />
+class Content extends Component {
+  render() {
+    return (
+      <>
+        <div className="ms-content-wrapper">
+          {/* Top Metric Cards (Entire Screen Width) */}
+          <div className="row no-gutters">
+            <div className="col-md-12">
+              <PaymentSummaryTable />
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div className="row">
-        <div className="col-12 mt-3">
-          <LabRecordTable />
-        </div>
-      </div>
+          <div className="row mt-3">
+            <div className="col-xl-7 col-md-12">
+              <Websiteaudience />
+              <div className="mt-3">
+                <ReportSummary />
+              </div>
+            </div>
 
-    </div>
-  );
-};
+            <div className="col-xl-5 col-md-12">
+              <DashboardCalendar />
+              <div className="mt-3">
+                <GoogleAnalytics />
+              </div>
+            </div>
+          </div>
+
+          {/* LabRecordTable and ReportSummary remain below */}
+          <div className="row">
+            <div className="col-12 mt-3">
+              <LabRecordTable />
+            </div>
+          </div>
+
+          {/* Existing second row unchanged 
+          <div className="row">
+            <Topuser />
+            <div className="col-xl-7 col-md-12">
+              <Averagenycountry />
+            </div>
+          </div>*/}
+        </div>
+      </>
+    );
+  }
+}
 
 export default Content;
